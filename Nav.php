ef4b2df2cf95ad522aabb084617ddc2f3b12f797
のぶ <nobuhiko.kimoto@gmail.com>
2020-04-23 10:29:19 +0900

    first commit

---
+++ Nav.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\ProductBrowsingHistory;
+
+use Eccube\Common\EccubeNav;
+
+class Nav implements EccubeNav
+{
+    /**
+     * @return array
+     */
+    public static function getNav()
+    {
+        return [];
+    }
+}
